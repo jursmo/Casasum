ce1fc7680e4074e8ab7e628b1fa918da19d1cba0
jursmo <juro.smotlak@centrum.sk>
2024-06-29 19:11:14 +0200

    - Založenie "view" sekcie v rámci projektu.  View sekcia bude slúžiť pre zdrojové kódy  vizualizácie. - Rozšírenie "namespace Casasum" o "view":  "namespace Casasum.view". - Presun (zatial základnej podoby) zdrojových  kódov vizualizácie do "view" sekcie. - Premenovanie zdrojových kódov z "Form1"  na "CasasumBaseForm".

---
+++ view/CasasumBaseForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Casasum.view
+{
+    public partial class casasumBaseForm : Form
+    {
+        public casasumBaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void casasumBaseForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
