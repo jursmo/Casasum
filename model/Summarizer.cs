5de468b69533f83818e0f224b47089d142ac1c1f
jursmo <juro.smotlak@centrum.sk>
2024-06-29 23:37:49 +0200

    Implementácia pomocnej triedy "Summarizer"
    - Trieda Summarizer bude vykonávať súčet položiek
    vytvorených pomocou dotazov "linq" zo spracovaných
    dát získaných zo vstupného XML súboru, a nad týmito
    dátami bude vytvárať celkový súčet "súm s dph" a
    "bez dph" pre jednotlivé položky zoznamu.

--- model/Summarizer.cs
+++ model/Summarizer.cs
@@ -8,6 +8,25 @@ namespace Casasum.model
 {
     public static class Summarizer
     {
-
+                public static Dictionary<string, Dictionary<string, double>>  saleSum( dynamic linqQuery )
+        {
+            Dictionary< string, Dictionary< string, double > > _saleSummary = new Dictionary< string, Dictionary< string, double > >();
+            var items = linqQuery;
+            foreach ( var item in items )
+            {
+                if ( _saleSummary.ContainsKey( item.saleCase.Model ) )
+                {
+                    _saleSummary[item.saleCase.Model]["priceWoVat"] += item.saleCase.PriceWoVat;
+                    _saleSummary[item.saleCase.Model]["priceWithVat"] += item.saleCase.PriceWithVat;
+                }
+                else
+                {
+                    _saleSummary.Add( item.saleCase.Model, new Dictionary<string, double>() );
+                    _saleSummary[ item.saleCase.Model ].Add( "priceWoVat",   item.saleCase.PriceWoVat   );
+                    _saleSummary[ item.saleCase.Model ].Add( "priceWithVat", item.saleCase.PriceWithVat );
+                }
+            }
+            return _saleSummary;
+        }
     }
 }
